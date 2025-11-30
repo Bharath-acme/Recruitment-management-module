9263a6bb07625b95ace75af2ca633f0abe065bcb
Bharath-acme <bharath.k@acmeglobal.tech>
2025-11-30 13:10:05 +0530

    updated migration files with new tables and modified env file

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,12 +3,18 @@ import sys
 from logging.config import fileConfig
 from sqlalchemy import create_engine, pool
 from alembic import context
+from candidates.models import *
+from requisitions.models import *
+from interviews.models import *
+from invoices.models import *
+from offers.models import *
+from vendors.models import *
 from app import models  # loads main models
 from app import __init__  #
 # Import Base + DATABASE_URL from your app
 from app.database import Base, DATABASE_URL
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "/home/site/wwwroot/backend"))
+sys.path.append("/home/site/wwwroot/backend")
 
 config = context.config
 
